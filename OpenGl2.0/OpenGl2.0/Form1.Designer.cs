f5759e20d1a687218b6faac0069bb510c805675b
STARLIGHT\Romeo <romeoxp777@gmail.com>
2017-02-24 12:27:53 +0200

    no message

---
+++ OpenGl2.0/OpenGl2.0/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace OpenGl2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.OpenGlo = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 327);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Remove Shape";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add Shape";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OpenGlo
+            // 
+            this.OpenGlo.AccumBits = ((byte)(0));
+            this.OpenGlo.AutoCheckErrors = false;
+            this.OpenGlo.AutoFinish = false;
+            this.OpenGlo.AutoMakeCurrent = true;
+            this.OpenGlo.AutoSwapBuffers = true;
+            this.OpenGlo.BackColor = System.Drawing.Color.Black;
+            this.OpenGlo.ColorBits = ((byte)(32));
+            this.OpenGlo.DepthBits = ((byte)(16));
+            this.OpenGlo.Location = new System.Drawing.Point(12, 12);
+            this.OpenGlo.Name = "OpenGlo";
+            this.OpenGlo.Size = new System.Drawing.Size(740, 309);
+            this.OpenGlo.StencilBits = ((byte)(0));
+            this.OpenGlo.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 362);
+            this.Controls.Add(this.OpenGlo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private Tao.Platform.Windows.SimpleOpenGlControl OpenGlo;
+    }
+}
+
---
+++ OpenGl2.0/OpenGl2.0/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+
+namespace OpenGl2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            OpenGlo.InitializeContexts();
+
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE);
+            Gl.glClearColor(255, 255, 255, 1);
+            Gl.glViewport(0, 0, OpenGlo.Width, OpenGlo.Height);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+
+            //  if (OpenGlo.Width <= OpenGlo.Height)
+            //     Glu.gluOrtho2D(-50, OpenGlo.Width, -50, 50.0 * (float)OpenGlo.Height / (float)OpenGlo.Width);
+            // else
+            //     Glu.gluOrtho2D(-50, 50.0 * (float)OpenGlo.Width / (float)OpenGlo.Height, -50, 50.0);
+
+
+            double aspect = OpenGlo.Width / (OpenGlo.Height);
+            if (OpenGlo.Width >= OpenGlo.Height)
+            {
+                Glu.gluOrtho2D(-50.0 * aspect, 50.0 * aspect, -50.0, 50.0);
+            }
+            else
+            {
+                Glu.gluOrtho2D(-50.0, 50.0, -50.0 / aspect, 50.0 / aspect);
+            }
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenGlo_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class Shape
+        {
+            public Point MainPoint;
+            public float[] ColorArray = new float[3];
+            public string NameOfShape;
+            public int KoefOfChange;
+        }
+
+        private List<Shape> Shapes = new List<Shape>();
+        private Random MyColor = new Random();
+        private void Draw(Shape s)
+        {
+            int CountOfVertex = 0;
+            if (s.NameOfShape == "Triangle") CountOfVertex = 3;
+            if (s.NameOfShape == "Quadrangle") CountOfVertex = 4;
+            if (s.NameOfShape == "Pentagon") CountOfVertex = 5;
+            if (s.NameOfShape == "Hexagon") CountOfVertex = 6;
+
+            Gl.glColor3d(s.ColorArray[0], s.ColorArray[1], s.ColorArray[2]);
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glVertex2d(s.MainPoint.X, s.MainPoint.Y);
+            Gl.glVertex2d(s.MainPoint.X + s.KoefOfChange, s.MainPoint.Y);
+            Gl.glVertex2d(s.MainPoint.X + s.KoefOfChange, s.MainPoint.Y - s.KoefOfChange);
+            if (CountOfVertex > 5)
+            {
+                Gl.glVertex2d(s.MainPoint.X + s.KoefOfChange * 0.7, s.MainPoint.Y - s.KoefOfChange * 1.7);
+                Gl.glVertex2d(s.MainPoint.X + s.KoefOfChange * 0.3, s.MainPoint.Y - s.KoefOfChange * 1.7);
+                Gl.glVertex2d(s.MainPoint.X, s.MainPoint.Y - s.KoefOfChange);
+            }
+            else
+            {
+                if (CountOfVertex == 4) Gl.glVertex2d(s.MainPoint.X, s.MainPoint.Y - s.KoefOfChange);
+                if (CountOfVertex == 5) Gl.glVertex2d(s.MainPoint.X + s.KoefOfChange * 0.5, s.MainPoint.Y + s.KoefOfChange);
+            }
+
+
+
+            Gl.glEnd();
+
+            Gl.glFlush();
+            Gl.glRasterPos2f(s.MainPoint.X, s.MainPoint.Y);
+            text(s.NameOfShape);
+            OpenGlo.Invalidate();
+        }
+        static void text(string c)
+        {
+            for (int i = 0; i < c.Length; i++)
+            {
+                // Render bitmap character 
+                Glut.glutBitmapCharacter(Glut.GLUT_BITMAP_TIMES_ROMAN_10, c[i]);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Shape s = new Shape();
+            Random rand = new Random();
+            s.ColorArray[0] = (float)MyColor.Next(0, 256) / 255;
+            s.ColorArray[1] = (float)MyColor.Next(0, 256) / 255;
+            s.ColorArray[2] = (float)MyColor.Next(0, 256) / 255;
+            int CountOfVertex = rand.Next(3, 7);
+            if (CountOfVertex == 3) s.NameOfShape = "Triangle";
+            if (CountOfVertex == 4) s.NameOfShape = "Quadrangle";
+            if (CountOfVertex == 5) s.NameOfShape = "Pentagon";
+            if (CountOfVertex == 6) s.NameOfShape = "Hexagon";
+            s.MainPoint.X = rand.Next(-30, 30);
+            s.MainPoint.Y = rand.Next(-30, 30);
+            s.KoefOfChange = rand.Next(-10, 10);
+            Shapes.Add(s);
+            for (int i = 0; i < Shapes.Count; i++)
+            {
+                Draw(Shapes[i]);
+            }
+
+            // Glut.glutDisplayFunc(Draw);
+            // OpenGlo.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            if (Shapes.Count != 0)
+            {
+                Shapes.RemoveAt(Shapes.Count - 1);
+
+                for (int i = 0; i < Shapes.Count; i++)
+                {
+                    Draw(Shapes[i]);
+                }
+                OpenGlo.Invalidate();
+            }
+            else
+            {
+                //OpenGlo.Invalidate();
+                MessageBox.Show("All shapes are removed");
+
+            }
+        }
+    }
+}
